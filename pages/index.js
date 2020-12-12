361046e4f862876326029b0a49d9d11726242761
bobzera <weslleyzera2020@gmail.com>
2020-12-12 18:33:15 +0000

    teste de commit 2

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function IndexPage() {
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-gray-900 dark:text-gray-100">
-          Teste de velocidade
+          Teste de commit pelo VSCODE
         </h1>
       </div>
     </div>
